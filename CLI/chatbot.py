cdf358ece636973949faa2cdbabd5588b5c42035
AyaanZaveri <ayaanzaveri08@gmail.com>
2021-11-20 14:56:09 -0500

    Chatbot Implementation

--- CLI/chatbot.py
+++ CLI/chatbot.py
@@ -1,9 +1,11 @@
 import openai
 
-openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key = "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"
+
 completion = openai.Completion()
 
-with open("samples.txt") as file:
+with open("CLI/samples.txt") as file:
 	default_log = "\n".join(file.readlines())
 
 def ask(question, chat_log):
@@ -13,13 +15,21 @@ def ask(question, chat_log):
 		prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
 		top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
 		max_tokens=150)
-	
+
 	answer = response.choices[0].text.strip()
 	return answer
 
 if __name__ == "__main__":
+	qLog = []
+	aLog = []
 	while True:
 		
 		question = input("Q: ")
 		answer = ask(question, default_log)
+
+		qLog.append(question)
+		aLog.append(answer)
+
+		print(str(qLog))
+		print(str(aLog))
 		print(answer)
--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import os
 app = Flask(__name__)
 app.secret_key = "abc"
 
+qLog = []
+aLog = []
+
 #if __name__ == "__main__":
 #    app.run()
 
@@ -18,32 +21,45 @@ def process():
   name = name.title()
   print(name)
 
-  openai.api_key = "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"
-  response = openai.Completion.create(
-  engine="davinci-codex",
-  prompt="Question: How big is the sun?\nAnswer: The sun is about 1.39 million kilometers (864,000 miles) in diameter. (The diameter we see is from Earth's perspective, but the sun is much bigger from the sun's perspective.)\n\nQuestion: How hot is the sun?\nAnswer: The temperature inside the sun is about 15 million degrees Celsius. (It is much hotter than that, but we cannot see the temperature from here because of the size of the sun.)\n\nQuestion: How many planets are there?\nAnswer: There are eight planets in our solar system.\n\nQuestion: Can we see the other planets?\nAnswer: Yes, we can see them with a telescope.\n\nQuestion: How far away is the sun?\nAnswer: The sun is 93 million miles away from Earth. (It is much farther away than that, but we cannot see the distance from here.)\n\nQuestion: What is the sun made of?\nAnswer: The sun is made mostly of hydrogen and a little bit of helium.\n\nQuestion: Why is the sky blue?\nAnswer: Blue is the color of the sky because of Earth's atmosphere.\n\nQuestion: Why are the sunrises and sunsets red?\nAnswer: When the sun goes down, it leaves behind red, orange, yellow, and purple color.\n\nQuestion: Why do we need the sun?\nAnswer: We need the sun for warmth and light.\n\nQuestion: How long does it take for the sun to circle Earth?\nAnswer: It takes the sun about a month to circle the Earth.\n\nQuestion: How far away is the moon?\nAnswer: The moon is about 230,000 miles away from Earth.\n\nQuestion: What is the moon made of?\nAnswer: The moon is made mostly of rock and dust.\n\nQuestion: How big is the moon?\nAnswer: The moon is about 2,000 miles across.\n\nQuestion: How many moons does Earth have?\nAnswer: Earth has one moon.\n\nQuestion: Why is the sky blue during the day?\nAnswer: The sky is blue during the day because of particles in the air.\n\nQuestion: " + name + "\n",
-  temperature=0,
-  max_tokens=1000,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=["\n"]
-)
+  openai.api_key = "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"
 
+  completion = openai.Completion()
 
-  #print(response)
+  with open("CLI/samples.txt") as file:
+    default_log = "\n".join(file.readlines())
 
-  res = json.loads(response.last_response.body)
-  print(res)
-  answer = res['choices'][0]['text']
+  def ask(question, chat_log):
+    
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+      prompt=prompt, engine="davinci-codex", stop=['\nHuman'], temperature=0.9,
+      top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+      max_tokens=150)
+
+    answer = response.choices[0].text.strip()
+    return answer
+
+  if __name__ == "__main__":
+      
+    question = name
+    answer = ask(question, default_log)
+
+    qLog.append(question)
+    aLog.append(answer)
+
+    qLogStr = str(qLog)
+    aLogStr = str(aLog)
+
+    print(qLogStr)
+    print(aLogStr)
+    print(answer)
+
+#  if answer == "'''":
+ #   answer = "Answer:"
+
   print(answer)
 
-  if answer == "'''":
-    answer = "Answer:"
-
-  print(answer)
-
-  return render_template('index.html', answer=answer, name=name)
+  return render_template('index.html', answer=answer, name=name, qLogStr=qLogStr, aLogStr=aLogStr)
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True, threaded=True, port=8181)
