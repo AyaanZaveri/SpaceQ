de97faf52c90fc77057a4119af4aa7a70ede6066
pickledAnchovies <93056983+pickledAnchovies@users.noreply.github.com>
2021-11-20 22:42:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template, session
-from flask import request
-import openai
-import json
 import requests 
 import os
 app = Flask(__name__)
 app.secret_key = "abc"
 
-qLog = []
-aLog = []
-
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
 
 @app.route('/', methods=['GET','POST'])
 
@@ -21,53 +14,37 @@ def process():
   name = name.title()
   print(name)
 
-  #To set API_KEY, type "export OPENAI_API_KEY=YOUR_KEY" in terminal
-  openai.api_key = os.getenv("OPENAI_API_KEY")
+  openai.api_key = os.getenv("OPEN_AI_KEY")
+
   completion = openai.Completion()
 
   with open("CLI/samples.txt") as file:
     default_log = "\n".join(file.readlines())
 
-    prompt = f'{default_log}Human: {name}\nAI:'
-
+  def ask(question, chat_log):
+    
+    prompt = f'{chat_log}Human: {question}\nAI:'
     response = completion.create(
-    prompt=prompt, engine="davinci-codex", stop=['\nHuman'], temperature=0.9,
-    top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-    max_tokens=150)
+      prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+      top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+      max_tokens=150)
 
-    response = openai.Completion.create(
-      engine="davinci-codex",
-      prompt=prompt,
-      temperature=0,
-      max_tokens=1000,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0,
-      stop=["\n"]
-    )
+    answer = response.choices[0].text.strip()
+    return answer
 
-    #print(response)
-
-    res = json.loads(response.last_response.body)
-    answer = res['choices'][0]['text']
+  if __name__ == "__main__":
+      
+    question = name
+    answer = ask(question, default_log)
 
     print(answer)
 
-    qLog.append(name)
-    aLog.append(answer)
+#  if answer == "'''":
+ #   answer = "Answer:"
 
-    qLogStr = str(qLog)
-    aLogStr = str(aLog)
+  print(answer)
 
-    print(qLogStr)
-    print(aLogStr)
-
-    if answer == "'''":
-      answer = "Answer:"
-
-    print(answer)
-
-  return render_template('index.html', answer=answer, name=name, qLogStr=qLogStr, aLogStr=aLogStr)
+  return render_template('index.html', answer=answer, name=name)
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", debug=True, threaded=True, port=5000)
+  app.run(host="0.0.0.0", debug=True, threaded=True, port=8181)
